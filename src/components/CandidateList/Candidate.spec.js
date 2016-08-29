0102be0c407ecfc5b5d9076dc105b9ad5f18ae71
Chris Alexander <git@chrisalexander.me.uk>
2016-08-29 22:54:12 +0100

    add tests for Candidate component

---
+++ src/components/CandidateList/Candidate.spec.js
@@ -0,0 +1,36 @@
+/* eslint-env mocha */
+var React = require('react');
+var expect = require('expect');
+var enzyme = require('enzyme');
+
+var Candidate = require('./Candidate');
+
+var mockDetals = {
+  name: 'Test Candidate',
+  birthDate: '1990-03-08'
+};
+var mockExperience = [
+  {
+    company: 'RobCo Industries Inc.',
+    position: 'Business Analyst',
+    startDate: '2012-08-05',
+    endDate: '2016-04-30',
+    summary: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+  }
+];
+
+describe('<Candidate>', function () {
+  var wrapper;
+  beforeEach(function () {
+    wrapper = enzyme.shallow(<Candidate details={mockDetals} experience={mockExperience}/>);
+  });
+
+  it('displays the candidate\'s name', function () {
+    expect(wrapper.find('h3').text()).toEqual(mockDetals.name);
+  });
+
+  it('does not show candidate details by default', function () {
+    expect(wrapper.find('.candidate--birthDate').prop('hidden')).toBe(true);
+    expect(wrapper.find('.candidate--details').prop('hidden')).toBe(true);
+  });
+});
